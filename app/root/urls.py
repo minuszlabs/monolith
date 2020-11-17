e23dae0b2b1fa4e059a74c9c24331ef4d0ac3e85
Michał Boruch <michal@boruch.xyz>
2020-11-17 22:09:51 +0100

    Configure dev and handle requests redirected from proxy (#12)
    * Configure service to use dev domain.

--- app/root/urls.py
+++ app/root/urls.py
@@ -18,11 +18,11 @@ from django.urls import path, include
 from api.base.router import router
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("api/v1/admin/", admin.site.urls),
     # Auth
-    path("api-auth/", include("rest_framework.urls")),
-    path("auth/", include("djoser.urls")),
-    path("auth/", include("djoser.urls.authtoken")),
+    path("api/v1/api-auth/", include("rest_framework.urls")),
+    path("api/v1/auth/", include("djoser.urls")),
+    path("api/v1/auth/", include("djoser.urls.authtoken")),
     # API
     path("api/", include(router.urls)),
 ]
